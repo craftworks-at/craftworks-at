898caf81829e8fd8f288a52c41c48d42542fd8a1
Isery <michael.hettegger@gmx.net>
2015-12-24 00:50:41 +0100

    setup with jekyll

---
+++ README.md
@@ -0,0 +1,9 @@
+craftworks homepage
+
+Setup:
+
+bundle install
+
+jekyll serve -w --baseurl=""
+
+go to localhost:4000
---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+ * 
+ * newHP
+ *
+ * Copyright (c) 2015 craftworks - https://craftworks.at
+ * All rights reserved.
+ */
+
+(function($) {
+
+
+
+})($);
