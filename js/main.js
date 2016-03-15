d8687b117cb1ca5c1d87ab23845a198bf5cfab24
Jakob Lahmer <jakob_l@gmx.at>
2016-03-15 17:19:47 +0100

    feat(what-antimations): brings slider layout and what-animations

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 /*
  * 
- * newHP
+ * craftworks.at
  *
  * Copyright (c) 2015 craftworks - https://craftworks.at
  * All rights reserved.
  */
 
-(function($) {
+var sectionWhat = {
+  swiper: null,
+
+  init: function(options) {
+    var that = this;
+    // init swiper
+    this.swiper = new Swiper(options.whatSlider, {
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      // autoplay: 5000,
+      loop: true
+    });
+
+    // init whatCategory click
+    $(options.whatCategory).on('click', function() {
+      $(options.whatCategory).removeClass('active');
+      $(this).addClass('active');
+      that.swiper.slideTo($(this).data('slide-index'))
+    });
+  }
+};
+
+// doc ready
+(function($, Swiper) {
+
+  var options = {};
+  options.whatSlider    = '.section-what--swiper';
+  options.whatCategory  = '.section-what--what';
 
 
+  // init sections
+  sectionWhat.init(options);
 
-})($);
+})($, window.Swiper);
